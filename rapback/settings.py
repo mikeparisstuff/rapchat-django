9a322ef2e73a76d5825ff9cdac16f71e3cdcc41e
Michael Paris <mlparis92@gmail.com>
2014-07-22 00:46:57 -0700

    fab2

--- rapback/settings.py
+++ rapback/settings.py
@@ -59,14 +59,15 @@ elif INSTANCE_ID == 'HEROKU':
     }
 elif INSTANCE_ID == 'PROD' :
     DATABASES = {
-    'default': {
-      'ENGINE': 'django.db.backends.postgresql_psycopg2',
-      'NAME': 'rapchat_prod',
-      'USER': 'rapback1',
-      'PASSWORD': os.environ.get('DB_PW'),
-      'HOST': 'localhost',
-      'PORT': '',
-    }
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': os.environ['RDS_DB_NAME'],                      # Or path to database file if using sqlite3.
+            # The following settings are not used with sqlite3:
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+            'PORT': os.environ['RDS_PORT'],                      # Set to empty string for default.
+        }
     }
 elif INSTANCE_ID == 'PPE':
     DATABASES = {
