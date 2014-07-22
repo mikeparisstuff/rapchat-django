bafcee380af9d0f78ff6adfa1f41f3e8e6639b54
Michael Paris <mlparis92@gmail.com>
2014-07-21 23:56:57 -0700

    fabdeploy

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,7 @@
-from fabric.api import local, settings, abort
+from fabric.api import *
 from fabric.contrib.console import confirm
+
+env.hosts = ['rapback_web']
 
 def test():
     with settings(warn_only=True):
@@ -7,8 +9,8 @@ def test():
     if result.failed and not confirm("Test failed. Continue anyway?"):
         abort("Aborting at user request.")
 
-def commit():
-    local('git add -p && git commit')
+def commit(message):
+    local('git add -p && git commit -m %s' % message)
 
 def push():
     local('git push origin')
@@ -16,9 +18,16 @@ def push():
 def merge_to_master(branch_name):
     local('git checkout master && git merge ' + branch_name)
 
-def prepare_deploy():
-    commit()
+def prepare_deploy(message):
+    print message
+    commit(message)
     push()
 
 def deploy():
-    code_dir = "~/rapback"
+    code_dir = "~/rapback-web"
+    with settings(warn_only=True):
+        if run("test -d %s" % code_dir).failed:
+            run("git clone git@github.com:mlp5ab/rapchat-django.git %s" % code_dir)
+    with cd(code_dir):
+        run("git pull")
+        run("touch app.wsgi")
