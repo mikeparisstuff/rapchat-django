a074bba382b3a260ea58e787940e7271d15c7f5b
Michael Paris <mlparis92@gmail.com>
2014-07-22 06:08:22 +0000

    Added fabfile

---
+++ fabfile.py
@@ -0,0 +1,24 @@
+from fabric.api import local, settings, abort
+from fabric.contrib.console import confirm
+
+def test():
+    with settings(warn_only=True):
+        result = local('python manage.py test rapback')
+    if result.failed and not confirm("Test failed. Continue anyway?"):
+        abort("Aborting at user request.")
+
+def commit():
+    local('git add -p && git commit')
+
+def push():
+    local('git push origin')
+
+def merge_to_master(branch_name):
+    local('git checkout master && git merge ' + branch_name)
+
+def prepare_deploy():
+    commit()
+    push()
+
+def deploy():
+    code_dir = "/"
